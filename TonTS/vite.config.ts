17595b172fb72a8acb22b0c5c529765f133cee83
bobo25695 <137033085+bobo25695@users.noreply.github.com>
2024-07-05 15:42:56 +0800

    Add GitHub Actions workflow for GitHub Pages deployment
    Add GitHub Actions workflow for GitHub Pages deployment

--- TonTS/vite.config.ts
+++ TonTS/vite.config.ts
@@ -4,6 +4,6 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/TONTSr/', // 设置基础路径为存储库名称
   plugins: [react(), nodePolyfills()],
-  base: '/',
 });
